88ddc6c1cfb944ed1797f546559624876e05cd47
arter97 <qkrwngud825@gmail.com>
2015-04-16 01:13:42 +0900

    Revert "Import memchr implementation from cortex-strings for Cortex A15"
    This reverts commit 9f7adb469946bb4375f9f4489517221702529740.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- libc/arch-arm/cortex-a15/cortex-a15.mk
+++ libc/arch-arm/cortex-a15/cortex-a15.mk
@@ -1,5 +1,4 @@
 libc_bionic_src_files_arm += \
-    arch-arm/cortex-a15/bionic/memchr.S \
     bionic/__memcpy_chk.cpp \
     arch-arm/bionic/memcpy.neon.S \
     arch-arm/cortex-a15/bionic/memset.S \
