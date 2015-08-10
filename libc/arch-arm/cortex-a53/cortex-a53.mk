75a8afbfa69ef7809d83df1cda566512d6005995
Noel Macwan <yulespencer@gmail.com>
2015-08-10 10:33:50 +0530

    cortex-a53: fix build
    https://github.com/temasek/android_bionic/commit/88ddc6c1cfb944ed1797f546559624876e05cd47

--- libc/arch-arm/cortex-a53/cortex-a53.mk
+++ libc/arch-arm/cortex-a53/cortex-a53.mk
@@ -13,6 +13,3 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/strlen.S \
     arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/krait/bionic/memmove.S
-
-libc_bionic_src_files_arm += \
-    arch-arm/cortex-a15/bionic/memchr.S
